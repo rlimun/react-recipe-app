4337eb04eb64560e1c25abc37f63f9b9d9e4f203
Richelle Limun <rlimun@indeed.com>
2023-12-27 21:09:33 -0800

    Add build directory

---
+++ build/precache-manifest.fd93ef597cabb511275a276dab3101a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d2bf737e1bc91b36ff3de2982a471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b652474e75d6f9797c82",
+    "url": "/static/css/main.df4ae0d6.chunk.css"
+  },
+  {
+    "revision": "dbf19fd96ea7ed36de7a",
+    "url": "/static/js/2.e1e0ccdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1e0ccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b652474e75d6f9797c82",
+    "url": "/static/js/main.e835d5cd.chunk.js"
+  },
+  {
+    "revision": "c8e5fd3b141acc9e29e7",
+    "url": "/static/js/runtime-main.e51d1157.js"
+  }
+]);
